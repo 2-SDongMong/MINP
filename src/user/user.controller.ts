8561cd1ba3cfcb1334153fd64f95ecc1583150f8
KimHyungJip <atathj@nate.com>
2023-03-05 05:19:55 +0900

    [Rebase1] [수정]:주석삭제

---
+++ src/user/user.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('/login')
+  async login(@Body() body) {
+    return await this.userService.login(body.email, body.password);
+  }
+
+  @Post('/signup')
+  async createUser(@Body() body) {
+    return await this.userService.createUser(
+      body.email,
+      body.name,
+      body.nickname,
+      body.address,
+      body.password,
+      body.phone_number,
+      body.referral_code,
+    );
+  }
+
+  @Put('/update')
+  updateUser() {
+    this.userService.updateUser('email', 'new_name', 'new_password');
+  }
+}
