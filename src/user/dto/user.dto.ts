d572a705303b03d03e336ac53957e2d2347c984a
KimHyungJip <atathj@nate.com>
2023-03-05 04:44:51 +0900

    [add]  user.dto

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,10 @@
+//전송받을 클래스 객체 선언
+export class CreateUserDto{
+    nickname: string;
+    name: string;
+    password: string;
+    email: string;
+    address: string;
+    phone_number: string;
+}
+export class UpdateUserDto{}
