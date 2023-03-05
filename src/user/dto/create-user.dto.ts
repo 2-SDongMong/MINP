627d82adff7ffb4ad39b49f68e8394a49acd4fde
KimHyungJip <atathj@nate.com>
2023-03-05 18:15:26 +0900

    [추가] : add signup dto, login dto // add lodash

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  readonly nickname: string;
+  @IsString()
+  readonly name: string;
+  @IsString()
+  readonly email: string;
+  @IsString()
+  readonly address: string;
+  @IsString()
+  readonly phone_number: string;
+  @IsString()
+  readonly password: string;
+  @IsString()
+  readonly referral_code: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Get, Post, Put } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { LoginUserDto } from "./dto/login-user.dto";
 import { UserService } from "./user.service";
 
 @Controller("user")
@@ -6,13 +8,13 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
     
   @Post("/login")
-  async login(@Body() body) {
-    return await this.userService.login(body.email, body.password);
+  async login(@Body() data:LoginUserDto) {
+    return await this.userService.login(data.email, data.password);
   }
 
   @Post("/signup")
-  async createUser(@Body() body) {
-    return await this.userService.createUser(body.email, body.name, body.nickname,body.address, body.password, body.phone_number,body.referral_code);
+  async createUser(@Body() data: CreateUserDto) {
+    return await this.userService.createUser(data.email, data.name, data.nickname,data.address, data.password, data.phone_number,data.referral_code);
   }
 
   @Put("/update")
