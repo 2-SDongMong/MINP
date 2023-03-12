a77b4551f07f00c19b21b54ad23238ee3962c934
DevOcean Han <soa6318@gmail.com>
2023-03-13 01:19:13 +0900

    [리팩토링] messages.module과 reqesuts.module에서 필요 없는 부분 삭제

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtConfigService } from '../config/jwt.config.service';
 import { Request } from './request.entity';
 import { RequestsController } from './requests.controller';
 import { RequestsService } from './requests.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Request]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useClass: JwtConfigService,
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([Request])],
   controllers: [RequestsController],
   providers: [RequestsService],
   //TODO: exports: [RequestsService],
