647540cb409eeadb4fccffb40b53f55646cecdb3
KimHyungJip <atathj@nate.com>
2023-03-13 20:47:01 +0900

    [추가] : 안 읽은 메시지 확인

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, Repository, Timestamp } from 'typeorm';
 import { Message } from './message.entity';
 
 @Injectable()
@@ -10,7 +10,7 @@ export class MessagesRepository extends Repository<Message> {
 
   async getMessageById(id: number): Promise<Partial<Message>> {
     const message = await this.createQueryBuilder('m')
-      .select(['m.message_id', 'm.sender_id', 'm.recipient_id', 'm.content'])
+      .select(['m.message_id', 'm.sender_id', 'm.recipient_id', 'm.content','m.read_at'])
       .leftJoin('m.send_user', 'sender')
       .leftJoin('m.receive_user', 'receiver')
       .addSelect(['sender.nickname', 'receiver.nickname'])
@@ -37,5 +37,24 @@ export class MessagesRepository extends Repository<Message> {
       .where('m.sender_id = :senderId', { senderId })
       .getMany();
     return messages;
+  }
+
+  async updateReadAt(id: number,read_at: Date){
+    const messages = await this.createQueryBuilder('m')
+      .update(Message)
+      .set({read_at:read_at})
+      .where({ message_id: id })
+      .execute();
+    return messages
+  }
+
+  async getUnreadMessages(userId){
+    const messages = await this.createQueryBuilder('m')
+      .leftJoin('m.send_user', 'sender')
+      .leftJoin('m.receive_user', 'receiver')
+      .addSelect(['sender.nickname', 'receiver.nickname'])
+      .where("m.sender_id = :userId OR m.read_at = :null", { sender_id: userId, read_at: null })
+      .getMany();
+    return messages
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -35,7 +35,11 @@ export class UsersController {
   // 유저 정보 조회
   @Get('/mypage')
   getUser(@Req() req) {
+<<<<<<< HEAD
     return this.userService.getUserById(req.user);
+=======
+    return this.usersService.getUserById(req.user);
+>>>>>>> 05d944a ([추가] : 안 읽은 메시지 확인)
   }
 
   // 유저 정보 수정
@@ -45,20 +49,20 @@ export class UsersController {
     @Param('id') userId: number,
     @Body() data: UpdateMypageDto
   ) {
-    return this.userService.updateUserById(req.user, userId, data);
+    return this.usersService.updateUserById(req.user, userId, data);
   }
 
   // 유저 정보 삭제
   @Delete('/mypage/:id')
   deleteUserInfo(@Req() req, @Param('id') userId: number) {
-    return this.userService.deleteUserById(req.user, userId);
+    return this.usersService.deleteUserById(req.user, userId);
   }
 
   // Admin page API
   // 가입 신청 대기 조회 API
   @Get('/admin')
   getAllUser(@Req() req) {
-    return this.userService.getUserByStatus(req.user);
+    return this.usersService.getUserByStatus(req.user);
   }
 
   // 가입 신청 승인 API
@@ -68,18 +72,18 @@ export class UsersController {
     @Param('id') userId: number,
     @Body() data: UpdateMemberDto
   ) {
-    return this.userService.accessMember(req.user, userId, data);
+    return this.usersService.accessMember(req.user, userId, data);
   }
 
   // 일반 회원 목록 조회 API
   @Get('/admin/member')
   getMember(@Req() req) {
-    return this.userService.getAllMember(req.user);
+    return this.usersService.getAllMember(req.user);
   }
 
   // 전체 회원 삭제 API
   @Delete('/admin/member/:id')
   deleteMember(@Req() req, @Param('id') userId: number) {
-    return this.userService.deleteMemberById(req.user, userId);
+    return this.usersService.deleteMemberById(req.user, userId);
   }
 }
