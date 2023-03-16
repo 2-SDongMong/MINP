c3f1edfe3ebba3ce42b4c445ac9017d3d1c3b05f
KimHyungJip <atathj@nate.com>
2023-03-16 20:54:23 +0900

    pull위한 커밋

--- src/share-modules/share-products/entities/share-products.entity.ts
+++ src/share-modules/share-products/entities/share-products.entity.ts
@@ -2,34 +2,35 @@ import { ProductsCategory } from '../../share-products-category/entities/product
 import { User } from '../../../users/user.entity';
 import {
   Column,
+  CreateDateColumn,
+  DeleteDateColumn,
   Entity,
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { ProductsTradeLocation } from 'src/share-modules/share-products-trade-location/entities/products-trade-location.entity';
-
 @Entity({ schema: 'mooin_cat', name: 'share_products' })
 export class Products {
   @PrimaryGeneratedColumn('uuid')
   id: string;
-
   @Column()
   title: string;
-
   @Column({ type: 'text' })
   description: string;
-
   @Column({ default: false })
   isTrade: boolean;
-
+  @Column({ nullable: true })
+  imageUrl: string;
   @ManyToOne(() => ProductsTradeLocation, { cascade: true, eager: true })
   productsTradeLocation: ProductsTradeLocation[];
-
   @ManyToOne(() => ProductsCategory, { eager: true })
   productsCategory: ProductsCategory;
-
   @ManyToOne(() => User, (user) => user.products, { cascade: true })
   @JoinColumn({ name: 'user_id' })
   user: User;
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+  createdAt: Date;
+  @DeleteDateColumn({ name: 'deleted_at', type: 'timestamp', nullable: true })
+  deletedAt: Date | null;
 }
