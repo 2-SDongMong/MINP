40cb2701a6659306919fda5bea8c50d246ff269b
poS2tives <positive.crong@gmail.com>
2023-03-20 16:46:57 +0900

    pull 받기 전

--- src/post-comments/post-comments.controller.ts
+++ src/post-comments/post-comments.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Logger, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { CreatePostCommentDto } from './dto/create-post-comment.dto';
+import { UpdatePostCommentDto } from './dto/update-post-comment.dto';
+//import { PostCommentsService } from './post-comments.service';
 
 @Controller('post-comments')
-export class PostCommentsController {}
+export class PostCommentsController {
+    
+}
+
+
+
--- src/post-comments/post-comments.service.ts
+++ src/post-comments/post-comments.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostsService } from 'src/posts/posts.service';
+import { Repository } from 'typeorm';
+import { CreatePostCommentDto } from './dto/create-post-comment.dto';
+import { UpdatePostCommentDto } from './dto/update-post-comment.dto';
+import { PostComment } from './post-comment.entity';
 
 @Injectable()
-export class PostCommentsService {}
+ export class PostCommentsService {
+
+}
