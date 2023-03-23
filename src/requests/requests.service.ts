2377685403a86b6ffb030a003ec474b9852a6a39
KimHyungJip <atathj@nate.com>
2023-03-23 14:00:42 +0900

    [추가]:품앗이페이지 캐싱 추가

--- src/requests/requests.service.ts
+++ src/requests/requests.service.ts
@@ -1,5 +1,7 @@
 import {
   BadRequestException,
+  CACHE_MANAGER,
+  Inject,
   Injectable,
   NotFoundException,
   UnauthorizedException,
@@ -10,44 +12,56 @@ import { Request } from './request.entity';
 import { CreateRequestDto } from './dto/create-request.dto';
 import { UpdateRequestDto } from './dto/update-request.dto';
 import _ from 'lodash';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class RequestsService {
   constructor(
     @InjectRepository(Request)
-    private requestsRepository: Repository<Request>
+    private requestsRepository: Repository<Request>,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache
   ) {}
 
   async getRequests() {
-    const request = await this.requestsRepository.find({
-      relations: {
-        user: {
-          cats: true,
-        },
-      },
-      select: {
-        user: {
-          nickname: true,
-          cats: {
-            image: true,
+    const value = await this.cacheManager.get(`all-requests`);
+    
+    if(!value){
+      const request = await this.requestsRepository.find({
+        relations: {
+          user: {
+            cats: true,
           },
         },
-        request_id: true,
-        reserved_begin_date: true,
-        reserved_end_date: true,
-        updated_at: true,
-        detail: true,
-        is_ongoing: true,
-      },
-      order: {
-        created_at: 'DESC',
-      },
-    });
-    return request;
+        select: {
+          user: {
+            nickname: true,
+            cats: {
+              image: true,
+            },
+          },
+          request_id: true,
+          reserved_begin_date: true,
+          reserved_end_date: true,
+          updated_at: true,
+          detail: true,
+          is_ongoing: true,
+        },
+        order: {
+          created_at: 'DESC',
+        },
+      });
+      await this.cacheManager.set(`all-requests`, request);
+
+      return request;
+    }
+    return value;
+    
   }
 
   async getRequestsPagination(page = 1, take = 8) {
-    const requests = await this.requestsRepository
+    const value = await this.cacheManager.get(`RequestsPagination`);
+    if(!value){ 
+      const requests = await this.requestsRepository
       .createQueryBuilder('r')
       .select()
       .leftJoin('r.user', 'user')
@@ -57,7 +71,12 @@ export class RequestsService {
       .skip((page - 1) * take)
       .take(take)
       .getMany();
-    return requests;
+      await this.cacheManager.set(`RequestsPagination`, requests);  
+      
+      return requests;
+    }
+    return value;
+    
   }
 
   async getRequestsByAddressBname(bname: string) {
@@ -83,7 +102,9 @@ export class RequestsService {
   }
 
   async getRequestsByAddressBnamePagination(bname: string, page = 1, take = 8) {
-    const requests = await this.requestsRepository
+    const value = await this.cacheManager.get(`AddressBname${bname}`);
+    if(!value){ 
+      const requests = await this.requestsRepository
       .createQueryBuilder('r')
       .select()
       .leftJoin('r.user', 'user')
@@ -94,43 +115,51 @@ export class RequestsService {
       .skip((page - 1) * take)
       .take(take)
       .getMany();
-    return requests;
+      await this.cacheManager.set(`AddressBname${bname}`, requests);  
+      
+      return requests;
+    }
+    return value;
+    
   }
 
   async getRequestById(id: number) {
-    const request = await this.requestsRepository.find({
-      where: { request_id: id },
-      relations: {
-        user: {
-          cats: true,
-        },
-      },
-      select: {
-        user: {
-          user_id: true,
-          nickname: true,
-          cats: {
-            name: true,
-            age: true,
-            gender: true,
-            neutered: true,
-            image: true,
-            character: true,
+    const value = await this.cacheManager.get(`request${id}`);
+    if(!value){ 
+      const request = await this.requestsRepository.find({
+        where: { request_id: id },
+        relations: {
+          user: {
+            cats: true,
           },
         },
-        request_id: true,
-        reserved_begin_date: true,
-        reserved_end_date: true,
-        detail: true,
-        is_ongoing: true,
-      },
-    });
-
-    if (_.isNil(request)) {
-      throw new NotFoundException(`Request article not found. id: ${id}`);
+        select: {
+          user: {
+            user_id: true,
+            nickname: true,
+            cats: {
+              name: true,
+              age: true,
+              gender: true,
+              neutered: true,
+              image: true,
+              character: true,
+            },
+          },
+          request_id: true,
+          reserved_begin_date: true,
+          reserved_end_date: true,
+          detail: true,
+          is_ongoing: true,
+        },
+      });
+      await this.cacheManager.set(`request${id}`, request);  
+      if (_.isNil(request)) {
+        throw new NotFoundException(`Request article not found. id: ${id}`);
+      }
+      return request;
     }
-
-    return request;
+    return value;
   }
 
   async createRequest(id: number, bodyData: CreateRequestDto) {
